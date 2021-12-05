795145bdee1231d2dbf782907fb9f0d9cebd610a
bdunne <bdunne6@gmail.com>
2021-12-04 23:05:30 -0800

    Got day 4 part 1 working.

--- aoc4.m
+++ aoc4.m
@@ -1,12 +1,27 @@
 clear;
-run(fullfile('inputs','day_4_test.m'));
+% run(fullfile('inputs','day_4_test.m'));
+% n = x.n;
+% b1 = x.b1;
+% b2 = x.b2;
+% b3 = x.b3;
+%b = cat(3,b1,b2,b3);
 
-n = x.n;
-b1 = x.b1;
-b2 = x.b2;
-b3 = x.b3;
+txt = read_txt('inputs\day_4.txt');
+txt_parts = strsplit(txt,['\r\n\r\n']);
 
-b = cat(3,b1,b2,b3);
+n  = str2double(strsplit(txt_parts{1},','));
+b_txt = txt_parts(2:end);
+
+b = [];
+for i1 = 1:numel(b_txt)
+    bi1  = str2double(strsplit(b_txt{i1},{' ','\r\n'}));
+    bi1 = bi1(~isnan(bi1));
+    bi1 = reshape(bi1,[5,5])';
+    b = cat(3,b,bi1);
+end
+
+
+
 for i1 = 1:numel(n)
     s = ismember(b,n(1:i1));
     [c_ind,bw] = find(squeeze(all(s,1)));
