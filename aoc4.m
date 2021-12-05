fb401b1c682eb17ae9014362712f97d8a454d207
bdunne <bdunne6@gmail.com>
2021-12-04 23:26:59 -0800

    Got day 4 part 2 working.

--- aoc4.m
+++ aoc4.m
@@ -1,12 +1,51 @@
 clear;
-% run(fullfile('inputs','day_4_test.m'));
-% n = x.n;
-% b1 = x.b1;
-% b2 = x.b2;
-% b3 = x.b3;
-%b = cat(3,b1,b2,b3);
+file_path = 'inputs\day_4.txt';
+[n,b] = load_input(file_path);
 
-txt = read_txt('inputs\day_4.txt');
+for i1 = 1:numel(n)
+    s = ismember(b,n(1:i1));
+    [c_ind,bw] = find(squeeze(all(s,1)));
+    if isempty(bw)
+        [r_ind,bw] = find(squeeze(all(s,2)));
+    end
+    w = any(all(s,1),'all')|any(all(s,2),'all');
+    if w
+        break;
+    end
+end
+b_w = b(:,:,bw);
+
+snum = sum(~s(:,:,bw).*b_w,'all');
+
+p1 = snum*n(i1);
+%part1 answer
+p1
+
+%start part2 
+for i1 = 1:numel(n)
+    s = ismember(b,n(1:i1));
+    c_w = any(squeeze(all(s,1)),1);
+    r_w = any(squeeze(all(s,2)),1);
+
+    w_last = w;
+    w = c_w|r_w;
+    
+    w_new = w&(~w_last);
+    if all(w)
+        break;
+    end
+    
+end
+b_w = b(:,:, w_new);
+
+snum = sum((~s(:,:,w_new)).*b_w,'all');
+
+p2 = snum*n(i1);
+p2
+
+
+function [n,b] = load_input(file_path)
+txt = read_txt(file_path);
 txt_parts = strsplit(txt,['\r\n\r\n']);
 
 n  = str2double(strsplit(txt_parts{1},','));
@@ -20,23 +59,10 @@ for i1 = 1:numel(b_txt)
     b = cat(3,b,bi1);
 end
 
-
-
-for i1 = 1:numel(n)
-    s = ismember(b,n(1:i1));
-    [c_ind,bw] = find(squeeze(all(s,1)));
-    if isempty(bw)
-        [r_ind,bw] = find(squeeze(all(s,2)));
-    end
-    
-    w = any(all(s,1),'all')|any(all(s,2),'all');
-    if w
-        break;
-    end
+% run(fullfile('inputs','day_4_test.m'));
+% n = x.n;
+% b1 = x.b1;
+% b2 = x.b2;
+% b3 = x.b3;
+% b = cat(3,b1,b2,b3);
 end
-b_w = b(:,:,bw);
-
-snum = sum(~s(:,:,bw).*b_w,'all');
-
-answer = snum*n(i1);
-answer
