88d630b78e77c7a46fb9016d235931cd7e7f39ad
bdunne <bdunne6@gmail.com>
2021-12-05 17:29:34 -0800

    Got python solution for day 3 working.

--- 2021/python/aoc3.py
+++ 2021/python/aoc3.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+def bin_array_to_int(x):
+    xs = ''.join(x.astype('str').tolist())
+    x_int = int(xs,2)
+    return  x_int
+
+#main script
 input_path = '../inputs/day_3.txt'
 lines = open(input_path).read().split('\n')
 
@@ -9,18 +15,30 @@ b = np.asarray(bchars,np.uint8)
 g_b = np.uint8(np.median(b,0))
 e_b = np.uint8(g_b!= 1)
 
-g_bs = ''.join(g_b.astype('str').tolist())
-e_bs = ''.join(e_b.astype('str').tolist())
-
-g = int(g_bs,2)
-e = int(e_bs,2)
+g = bin_array_to_int(g_b)
+e = bin_array_to_int(e_b)
 
 p1 = g*e
+#part 1
 print(p1)
+#4191876
 
-#b1 = bin(1024)
-#x = np.array([1,0,0,0,0,0,0,0,0,0,0], dtype=np.bool)
-#px = np.packbits(x)
+#part 2
+o_bit_criteria = lambda x: np.uint8(np.median(x) > 0)
+co2_bit_criteria = lambda x: np.uint8(not o_bit_criteria(x))
 
-#v1 = np.flip(px).transpose().view(np.int16)
-#print(v1)
+def iterate_bit_criteria(b,bit_criteria):
+    for i1 in range(0,b.shape[1]):
+        ci1 = b[:,i1]
+        v_keep  = bit_criteria(ci1)
+        i_keep = ci1 == v_keep
+        b = b[i_keep,:]
+        if b.shape[0] == 1:
+            return b[0]
+    
+o_b = iterate_bit_criteria(b,o_bit_criteria)
+co2_b = iterate_bit_criteria(b,co2_bit_criteria)
+
+o = bin_array_to_int(o_b)
+co2 = bin_array_to_int(co2_b)
+print(o*co2)
