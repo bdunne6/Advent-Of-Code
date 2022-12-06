e091ee58e4790a0d0121141d44d09e871fdeb9f1
bdunne <bdunne6@gmail.com>
2022-12-05 22:36:55 -0800

    Minor comment change.

--- 2022/python/aoc6.py
+++ 2022/python/aoc6.py
@@ -1,9 +1,10 @@
 input_path = '../inputs/day_6.txt'
 x = open(input_path).read()
 
-#part 1
+#part 1,2
 for n in [4,14]:
     for i in range(0,len(x)-n):
         if len(set(x[i:i+n])) == n:
-            print(i+n)
+            print(i+n) 
             break
+            
