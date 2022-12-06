9244d76cfb1efdf139ddde3e357b06bf686e35b2
bdunne <bdunne6@gmail.com>
2022-12-05 22:30:14 -0800

    Simplify day6 python.

--- 2022/python/aoc6.py
+++ 2022/python/aoc6.py
@@ -2,21 +2,8 @@ input_path = '../inputs/day_6.txt'
 x = open(input_path).read()
 
 #part 1
-n = 4
-for i in range(0,len(x)-n):
-    i_end = i+n
-    ci = x[i:i_end]
-    if len(set(ci)) == n:
-        print(i_end)
-        break
-    
-#part 2
-n = 14
-for i in range(0,len(x)-n):
-    i_end = i+n
-    ci = x[i:i_end]
-    if len(set(ci)) == n:
-        print(i_end)
-        break
-        
-    
+for n in [4,14]:
+    for i in range(0,len(x)-n):
+        if len(set(x[i:i+n])) == n:
+            print(i+n)
+            break
