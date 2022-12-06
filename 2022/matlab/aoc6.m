2b1352aea8e50289c4e6ea4b65ffccdd26149bb0
bdunne <bdunne6@gmail.com>
2022-12-05 22:22:11 -0800

    Simplify day.

--- 2022/matlab/aoc6.m
+++ 2022/matlab/aoc6.m
@@ -1,37 +1,11 @@
 input_path = fullfile(get_input_root,'day_6.txt');
 x = read_txt(input_path);
-
-%part 1
-for i1 = 1:numel(x)
-    i_end = i1+3;
-    if i_end > numel(x)
-        break;
+%parts 1,2
+for n = [4,14]
+    for i1 = 1:numel(x)
+        if numel(unique(x(i1:i1+n-1))) == n
+            disp(i1 + n-1);
+            break
+        end
     end
-    
-    xi1 = x(i1:i_end);
-    if numel(unique(xi1)) ==4
-        istart = i1;
-        istart + 3
-        %xi1
-        break
-    end
-    
 end
-
-%part 2
-for i1 = 1:numel(x)
-    i_end = i1+13;
-    if i_end > numel(x)
-        break;
-    end
-    
-    xi1 = x(i1:i_end);
-    if numel(unique(xi1)) ==14
-        istart = i1;
-        istart + 13
-        %xi1
-        break
-    end
-    
-end
-
