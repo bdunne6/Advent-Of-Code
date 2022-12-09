f1a561566b3be3a94cee06ebadffadd22ce8705b
bdunne <bdunne6@gmail.com>
2022-12-08 21:29:16 -0800

    Add part 2 solution.

--- 2022/matlab/aoc9.m
+++ 2022/matlab/aoc9.m
@@ -1,12 +1,13 @@
 input_path = fullfile(get_input_root,'day_9.txt');
 x = readcell(input_path);
 
+%% part 1 
 h_path = [0,0];
 t_path = [0,0];
 
 h = [0,0];
 t = [0,0];
-figure;
+
 for i1 = 1:size(x,1)
     n_s = x{i1,2};
     s_h = get_xy_step(x{i1,1});
@@ -22,19 +23,38 @@ for i1 = 1:size(x,1)
         
         t = t + s_t;
         
-%         hold off;
-%         plot(h(1),h(2),'.r');
-%         hold on;
-%         plot(t(1),t(2),'ob');
-%         pause(0.5)
-%         
-        
         h_path = cat(1,h_path,h);
         t_path = cat(1,t_path,t);
     end
 end
-size(unique(t_path,'rows'))
+p1 = size(unique(t_path,'rows'),1);
+disp(p1)
 
+%% part 2
+k = zeros(10,2);
+t_path = [];
+for i1 = 1:size(x,1)
+    n_s = x{i1,2};
+    s_h = get_xy_step(x{i1,1});
+    for i2 = 1:n_s
+        k(1,:) = k(1,:) + s_h;
+        for i3 = 2:size(k,1)
+        d = k(i3-1,:) - k(i3,:);
+        
+        if max(abs(d))>1
+            s_t = sign(d);
+        else
+            s_t = [0,0];
+        end
+        
+        k(i3,:) = k(i3,:) + s_t;
+        end
+        t_path = cat(1,t_path,k(end,:));
+    end
+end
+
+p2 = size(unique(t_path,'rows'),1);
+disp(p2)
 
 function s = get_xy_step(c)
 switch c
