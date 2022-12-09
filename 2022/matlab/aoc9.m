22a061daebd51e956c4ab21c3b807630513954da
bdunne <bdunne6@gmail.com>
2022-12-09 07:59:39 -0800

    Make day 9 solution more compact.

--- 2022/matlab/aoc9.m
+++ 2022/matlab/aoc9.m
@@ -1,70 +1,28 @@
 input_path = fullfile(get_input_root,'day_9.txt');
 x = readcell(input_path);
+step_lookup = struct('R',[1,0],'L',[-1,0],'U',[0,1],'D',[0,-1]);
 
-%% part 1 
-h_path = [0,0];
-t_path = [0,0];
-
-h = [0,0];
-t = [0,0];
-
-for i1 = 1:size(x,1)
-    n_s = x{i1,2};
-    s_h = get_xy_step(x{i1,1});
-    for i2 = 1:n_s
-        h = h + s_h;
-        d = h - t;
-        
-        if max(abs(d))>1
-            s_t = sign(d);
-        else
-            s_t = [0,0];
+%% parts 1,2
+for n = [2,10]
+    k = zeros(n,2);
+    t_path = nan(sum(cat(1,x{:,2})),2);
+    ip = 0;
+    for i1 = 1:size(x,1)
+        for i2 = 1:x{i1,2}
+            k(1,:) = k(1,:) + step_lookup.(x{i1,1});
+            for i3 = 2:size(k,1)
+                d = k(i3-1,:) - k(i3,:);
+                if max(abs(d))>1
+                    s_t = sign(d);
+                else
+                    s_t = [0,0];
+                end
+                k(i3,:) = k(i3,:) + s_t;
+            end
+            ip = ip+1;
+            t_path(ip,:) = k(end,:);
         end
-        
-        t = t + s_t;
-        
-        h_path = cat(1,h_path,h);
-        t_path = cat(1,t_path,t);
     end
-end
-p1 = size(unique(t_path,'rows'),1);
-disp(p1)
-
-%% part 2
-k = zeros(10,2);
-t_path = [];
-for i1 = 1:size(x,1)
-    n_s = x{i1,2};
-    s_h = get_xy_step(x{i1,1});
-    for i2 = 1:n_s
-        k(1,:) = k(1,:) + s_h;
-        for i3 = 2:size(k,1)
-        d = k(i3-1,:) - k(i3,:);
-        
-        if max(abs(d))>1
-            s_t = sign(d);
-        else
-            s_t = [0,0];
-        end
-        
-        k(i3,:) = k(i3,:) + s_t;
-        end
-        t_path = cat(1,t_path,k(end,:));
-    end
-end
-
-p2 = size(unique(t_path,'rows'),1);
-disp(p2)
-
-function s = get_xy_step(c)
-switch c
-    case 'R'
-        s= [1,0];
-    case 'L'
-        s= [-1,0];
-    case 'U'
-        s= [0,1];
-    case 'D'
-        s= [0,-1];
-end
+    p2 = size(unique(t_path,'rows'),1);
+    disp(p2)
 end
