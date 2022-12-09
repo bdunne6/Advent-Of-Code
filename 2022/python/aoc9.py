9d9af39d492c43dfddcaf20d58a27e2aed9fd081
bdunne <bdunne6@gmail.com>
2022-12-09 09:02:49 -0800

    Make day 9 python solution more compact.

--- 2022/python/aoc9.py
+++ 2022/python/aoc9.py
@@ -14,7 +14,7 @@ step_lookup = {'R':np.array([1,0]),'L':np.array([-1,0]),'U':np.array([0,1]),'D':
 for n in [2,10]:
     k = np.zeros((n,2))
     t_path = np.zeros((sum(moves_sizes),2))
-    ip = 0
+    ip = -1
     for m in moves:
         for j in range(0,m[1]):
             k[0,:] += step_lookup[m[0]]
@@ -22,7 +22,5 @@ for n in [2,10]:
                 d = k[i-1,:] - k[i,:]
                 s_t = np.sign(d) if max(abs(d))>1 else np.array([0,0]) 
                 k[i,:] += s_t
-            t_path[ip,:] =  k[-1,:]
-            ip += 1
-    urows = np.unique(np.array(t_path),axis=0)
-    print(urows.shape[0])
+            t_path[(ip := ip + 1),:] =  k[-1,:]
+    print(np.unique(np.array(t_path),axis=0).shape[0])
