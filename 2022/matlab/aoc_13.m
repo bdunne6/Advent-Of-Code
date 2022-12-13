504c646fb178d4de89146b4a0f67e88249f64438
bdunne <bdunne6@gmail.com>
2022-12-12 23:32:51 -0800

    Add day 13 part 2 solution.

--- 2022/matlab/aoc_13.m
+++ 2022/matlab/aoc_13.m
@@ -8,25 +8,48 @@ etxt = strrep(etxt,']','}');
 
 eval(etxt);
 n_pairs = numel(x)/2;
+
+%% part 1
 r = zeros(n_pairs,1);
 for i1 = 1:n_pairs
-    disp('%%%%%%%%%%%%%%%%%%%%')
     ind = 2*i1-1;
-    
     x1 = x{ind};
     x2 = x{ind+1};
     r(i1) = compare(x1,x2);
-    
 end
 p1 = sum(find(r));
 disp(p1)
 
+%% part 2
+dp1 = {{2}};
+dp2 = {{6}};
+xs = cat(1,x,dp1,dp2);
+xs = insertion_sort(xs,@compare);
+xs = flipud(xs);
+
+i_dp = [];
+for i1 = 1:numel(xs)
+   if isnan(compare(xs(i1),dp1))
+       i_dp = [i_dp,i1];
+   elseif isnan(compare(xs(i1),dp2))
+       i_dp = [i_dp,i1];
+   end
+end
+p2 = prod(i_dp);
+disp(p2)
+
+function r = sort_func(x1,x2)
+r = compare(x1,x2);
+if r == 0
+    r = -1;
+elseif isnan(r)
+    r = 0;
+end
+end
+
 function r = compare(x1,x2)
 r = nan;
 for i1 = 1:min(numel(x1),numel(x2))
-    disp('comparing:')
-    disp(x1{i1})
-    disp(x2{i1})
     if isnumeric(x1{i1})&&isnumeric(x2{i1})
         if x1{i1} < x2{i1}
             r = 1;
@@ -40,7 +63,6 @@ for i1 = 1:min(numel(x1),numel(x2))
     elseif iscell(x1{i1})&&iscell(x2{i1})
         r = compare(x1{i1},x2{i1});
     end
-    
     if ~isnan(r)
         return;
     end
@@ -54,4 +76,22 @@ if isnan(r)
         return;
     end
 end
+end
+
+function [x] = insertion_sort(x,cfun)
+%insertion_sort with optional comparison function
+if nargin < 2
+   cfun = @(x,y) (x > y); 
+end
+i = 2;
+while i <= length(x)
+    j = i;
+    while j > 1 && cfun(x(j-1),x(j))
+        t =  x(j);
+        x(j) = x(j-1);
+        x(j-1) = t;
+        j = j - 1;
+    end 
+    i = i + 1;
+end 
 end
