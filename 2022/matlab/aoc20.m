d2ac692d9d0fd9934a4ba63e0e9282c90442b82e
bdunne <bdunne6@gmail.com>
2022-12-22 00:02:49 -0800

    Modification for part 2.

--- 2022/matlab/aoc20.m
+++ 2022/matlab/aoc20.m
@@ -1,11 +1,15 @@
 input_path = fullfile(get_input_root,'day_20.txt');
 x = readmatrix(input_path);
 
+x = int64(x);
+x = x*811589153;
+
 i = [1:numel(x)]';
 x0 = x;
 i0 = i;
 
 n = numel(x0);
+for k = 1:10
 for i1 = 1:numel(i0)
    i_m = find(i0(i1) == i);
 
@@ -25,11 +29,12 @@ for i1 = 1:numel(i0)
    x = [ x(ir(ir<i_n)) ; v; x(ir(ir>=i_n))];
    i = [ i(ir(ir<i_n)) ; iv ;i(ir(ir>=i_n))];
 end
+end
 
 iz = find(x == 0);
 i_sum = wrap(iz + [1000,2000,3000],n);
 
-sum(x(i_sum))
+disp(sum(x(i_sum)))
 
 
 
