16a1ca9760b42df481aaa6d73876e77204358505
bdunne <bdunne6@gmail.com>
2022-12-22 23:11:23 -0800

    Updated for day 23 part 2.

--- 2022/matlab/aoc23.m
+++ 2022/matlab/aoc23.m
@@ -12,7 +12,7 @@ for i1 = 1:numel(lines)
 end
 
 [y,x] = find(x);
-xy = [x,max(y)-y];
+xy = int64([x,max(y)-y]);
 
 %% set initial directions
 m = [0,1;%N
@@ -31,56 +31,29 @@ n  = [1,0;%N
 
 mc = {[0,1; 1,1; -1,1],[0,-1; 1,-1; -1,-1],[-1,0;-1,1;-1,-1],[1,0;1,1;1,-1]};
 
+m = int64(m);
+n = int64(n);
+mc = cellfun(@int64,mc,'UniformOutput',false);
 
-%% part 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% n_iter = 10;
-% for i0 = 1:n_iter
-%     xy_p = zeros(size(xy));
-%     i_m = false(size(xy,1),1);
-%     for i1 = 1:size(xy)
-%         xy1 = xy(i1,:);
-%         if any(ismember(xy1 + n,xy,'rows'))
-%             for i2 = 1:size(m,1)
-%                 xy_t = xy1 + mc{i2};
-%                 if ~any(ismember(xy_t,xy,'rows'))
-%                     i_m(i1) = true;
-%                     xy_p(i1,:) = xy1 + m(i2,:);
-%                     break;
-%                 end
-%             end
-%         end
-%     end
-%     rc_pc = xy_p(:,1) + xy_p(:,2)*1i;
-%     up = unique(rc_pc);
-%     for i1 = 1:numel(up)
-%         imu = up(i1) == rc_pc;
-%         if sum(imu) > 1
-%            i_m(imu) = false;
-%         end
-%     end
-% 
-%     xy(i_m,:) = xy_p(i_m,:);
-%     
-%     m = circshift(m,-1);
-%     mc = circshift(mc,-1);
-% end
-% 
-% xw = max(xy(:,1))- min(xy(:,1)) + 1;
-% yw = max(xy(:,2))- min(xy(:,2)) + 1;
-% 
-% et = xw*yw - size(xy,1);
+%% parts 1 and 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+n_iter = 1000000;
 
-%% part 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-n_iter = 10000000;
+y_off = int64(Inf)/2;
+tic
+k = get_keys(xy);
+xy_d = containers.Map(k,zeros(size(xy,1),1));
 for i0 = 1:n_iter
     xy_p = zeros(size(xy));
     i_m = false(size(xy,1),1);
     for i1 = 1:size(xy)
         xy1 = xy(i1,:);
-        if any(ismember(xy1 + n,xy,'rows'))
+        %if any(ismember(xy1 + n,xy,'rows'))
+        if any(xy_d.isKey(num2cell(get_keys(xy1 + n))))
             for i2 = 1:size(m,1)
                 xy_t = xy1 + mc{i2};
-                if ~any(ismember(xy_t,xy,'rows'))
+                %if ~any(ismember(xy_t,xy,'rows'))
+                
+                if ~any(xy_d.isKey(num2cell(get_keys(xy_t))))
                     i_m(i1) = true;
                     xy_p(i1,:) = xy1 + m(i2,:);
                     break;
@@ -93,20 +66,46 @@ for i0 = 1:n_iter
     for i1 = 1:numel(up)
         imu = up(i1) == rc_pc;
         if sum(imu) > 1
-           i_m(imu) = false;
+            i_m(imu) = false;
         end
     end
-
+    
+    if ~any(i_m)||all(xy_p(i_m,:) == xy(i_m,:),'all')
+        break;
+    end
+    
+    xy0 = xy; 
     xy(i_m,:) = xy_p(i_m,:);
+    
+    if all(xy0 == xy,'all')
+        break;
+    end
+    
+    xy_d = containers.Map(get_keys(xy),cell(size(xy,1),1));
     
     m = circshift(m,-1);
     mc = circshift(mc,-1);
-   
-    if ~any(i_m)
-       break; 
+    
+
+    
+    if i0 == 10
+        xw = max(xy(:,1))- min(xy(:,1)) + 1;
+        yw = max(xy(:,2))- min(xy(:,2)) + 1;
+        
+        p1 = xw*yw - size(xy,1);
+        disp(p1)
     end
+    
+end
+p2 = i0;
+disp(p2);
+
+
+
+function k = get_keys(xy)
+k = xy(:,1)+xy(:,2)*int64(2^32);
 end
 
-disp(i0)
+
 
 
