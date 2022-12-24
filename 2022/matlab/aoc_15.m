688e0aca4c47d9ec14d88b655136891bff49a639
bdunne <bdunne6@gmail.com>
2022-12-24 01:05:07 -0800

    Add part 2.

--- 2022/matlab/aoc_15.m
+++ 2022/matlab/aoc_15.m
@@ -28,8 +28,9 @@ else
 end
 s_d = sum(abs((s_xy - b_xy)),2);
 
-r_o = false(1,numel(xr));
 
+%% part 1  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+r_o = false(1,numel(xr));
 t0 = tic();
 for i1 = 1:numel(xr)
     x_q = xr(i1);
@@ -46,10 +47,74 @@ end
 xy_q = [xr(:),y_q*ones(numel(xr),1)];
 i_m = ismember(xy_q,b_xy,'rows');
 r_o(i_m) = false;
-t1 = toc(t0)
+t1 = toc(t0);
 
 sum(r_o)
 
+%% part 2  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+close all;
+figure;
+xy_di = [];
+xy_pc = cell(size(s_xy,1));
+xy_pa = [];
+
+t0 = tic();
+for i1 = 1:size(s_xy,1)
+    di1 = s_d(i1);
+    
+%     hold on;
+%     plot(s_xy(i1,1),s_xy(i1,2),'.r');
+    
+    dp = di1 + 1;
+    xp = -dp:dp;
+    yp = abs(xp)-dp;
+    %xp = [xp,xp];
+    xp = [xp,xp]+s_xy(i1,1);
+    yp = [yp,-yp]+s_xy(i1,2);
+    
+    %abs(xp) + abs(yp)
+    
+    plot(xp,yp,'.')
+    xy_p = [xp(:),yp(:)];
+    %xy_pc{i1} = xy_p;
+    xy_pa  = cat(1,xy_pa, xy_p);
+    
+%     if i1 > 1
+%         xy_d = intersect(xy_d,xy_p);
+%     else
+%         xy_d = xy_p;
+%     end
+    
+    %xy_d0 = xy_d;
+    
+
+
+end
+toc(t0)
+
+figure;
+    plot(xy_pa(:,1),xy_pa(:,2),'.');
+xr = [0,4000000];
+yr = [0,4000000];
+
+i_v =(xy_pa(:,1) >= xr(1))&(xy_pa(:,1) <= xr(2))&(xy_pa(:,2) >= yr(1))&(xy_pa(:,2) <= yr(2));
+
+for i1 = 1:size(s_xy,1)
+d_i1 = sum(abs((s_xy(i1,:) - xy_pa)),2);
+i_v = i_v&(d_i1>s_d(i1));
+end
+
+xy_b = xy_pa(i_v,:);
+xy_b = unique(xy_b,'rows');
+
+p2 = xy_b(1)*4000000 + xy_b(2);
+disp(p2)
+% xy_q = [xr(:),y_q*ones(numel(xr),1)];
+% i_m = ismember(xy_q,b_xy,'rows');
+% r_o(i_m) = false;
+% t1 = toc(t0);
+% 
+% sum(r_o)
 
 
 
