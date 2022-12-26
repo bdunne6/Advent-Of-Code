dade7c9a100899b796a1f70a8a0f49cac52337ae
bdunne <bdunne6@gmail.com>
2022-12-26 11:01:59 -0800

    Add parts 1 and 2 separately for day 20.

--- 2022/matlab/aoc20.m
+++ 2022/matlab/aoc20.m
@@ -1,42 +1,56 @@
 input_path = fullfile(get_input_root,'day_20.txt');
-x = readmatrix(input_path);
-
-x = int64(x);
-x = x*811589153;
-
-i = [1:numel(x)]';
-x0 = x;
-i0 = i;
+x0 = readmatrix(input_path);
 
 n = numel(x0);
-for k = 1:10
-for i1 = 1:numel(i0)
-   i_m = find(i0(i1) == i);
+x = int64(x0);
 
-   v = x(i_m);
-   iv = i(i_m);
-   x(i_m) = [];
-   i(i_m) = [];
-   ir = 1:numel(x0)-1;
-   
-   i_nu = i_m+v;
-   i_n = wrap(i_nu,numel(x));
-   
-   if (i_n == 1)&&v<0
-      i_n = numel(x0);
-   end
+i = (1:numel(x))';
+format longg
 
-   x = [ x(ir(ir<i_n)) ; v; x(ir(ir>=i_n))];
-   i = [ i(ir(ir<i_n)) ; iv ;i(ir(ir>=i_n))];
+%% part 1
+i0 = i;
+for i1 = 1:n
+    i_o = find(i0(i1) == i);
+    xv = x(i_o);
+    iv = i(i_o);
+    i_nu = i_o+xv;
+    i_n = wrap(i_nu,n-1);
+    if (i_n == 1)&&xv<0
+        i_n = n;
+    end
+    ir = 1:n-1;
+    x(i_o) = [];
+    i(i_o) = [];
+    x = [ x(ir(ir<i_n)) ; xv; x(ir(ir>=i_n))];
+    i = [ i(ir(ir<i_n)) ; iv ;i(ir(ir>=i_n))];
 end
-end
-
 iz = find(x == 0);
 i_sum = wrap(iz + [1000,2000,3000],n);
-
 disp(sum(x(i_sum)))
 
-
+%% part 2
+x = int64(x0)*811589153;
+i0 = i;
+for k = 1:10
+    for i1 = 1:n
+        i_o = find(i0(i1) == i);
+        xv = x(i_o);
+        iv = i(i_o);
+        i_nu = i_o+xv;
+        i_n = wrap(i_nu,n-1);
+        if (i_n == 1)&&xv<0
+            i_n = n;
+        end
+        ir = 1:n-1;
+        x(i_o) = [];
+        i(i_o) = [];
+        x = [ x(ir(ir<i_n)) ; xv; x(ir(ir>=i_n))];
+        i = [ i(ir(ir<i_n)) ; iv ;i(ir(ir>=i_n))];
+    end
+end
+iz = find(x == 0);
+i_sum = wrap(iz + [1000,2000,3000],n);
+disp(sum(x(i_sum)))
 
 function j = wrap(i,n)
 j = mod(i-1,n)+1;
