ac521df94ad0ddb566f39bd4ef1021b80ee2e88a
bdunne <bdunne6@gmail.com>
2023-12-03 21:44:57 -0800

    Simplify day 4.

--- 2023/matlab/aoc4.m
+++ 2023/matlab/aoc4.m
@@ -6,43 +6,24 @@ lines = strsplit(x,newline);
 
 %% part 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 p1 = 0;
-for i1_card = 1:numel(lines)
-    lp = strsplit(lines{i1_card},'|');
-    wn = strsplit(strip(extractAfter(lp{1},':')),' ');
-    cn = strsplit(strip(lp{2}),' ');
-    wn = str2double(wn);
-    cn = str2double(cn);
-    
-    nw = sum(ismember(cn,wn));
-    if nw > 0
-        v = 2^(nw-1);
-        p1 = v + p1;
-    end
+nw = zeros(numel(lines,1));
+for i1 = 1:numel(lines)
+    lp = strsplit(lines{i1},'|');
+    wn = str2double(strsplit(strip(extractAfter(lp{1},':')),' '));
+    cn = str2double(strsplit(strip(lp{2}),' '));
+    nw(i1) = sum(ismember(cn,wn));
+    p1 = (2^(nw(i1)-1))*(nw(i1) > 0) + p1;
 end
 disp(p1)
 
 %% part 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-card_ids = 1:numel(lines);
-m0 = containers.Map(card_ids,zeros(size(card_ids)));
-for i1_card = 1:numel(lines)
-    lp = strsplit(lines{i1_card},'|');
-    wn = strsplit(strip(extractAfter(lp{1},':')),' ');
-    cn = strsplit(strip(lp{2}),' ');
-    wn = str2double(wn);
-    cn = str2double(cn);
-    
-    nw = sum(ismember(cn,wn));
-    
-    cards_won = i1_card+1:i1_card+nw;
-    m0(i1_card) = m0(i1_card) + 1;
-    for i2 = 1:numel(cards_won)
-        i2_card = cards_won(i2);
-        m0(i2_card) = m0(i2_card)+m0(i1_card);
+n_cards = zeros(numel(lines),1);
+for i1 = 1:numel(lines)
+    c_won = i1+1:i1+nw(i1);
+    n_cards(i1) = n_cards(i1) + 1;
+    for i2 = 1:numel(c_won)
+        n_cards(c_won(i2)) = n_cards(c_won(i2))+n_cards(i1);
     end
 end
-vals = m0.values;
-p2 = sum([vals{:}]);
+p2 = sum(n_cards);
 disp(p2)
-
-
-
