67167da31e62c7749faad81f8b6b50bdd3553808
bdunne <bdunne6@gmail.com>
2024-12-06 22:41:26 -0800

    2024 day 7 improvements.

--- 2024/python/aoc7.py
+++ 2024/python/aoc7.py
@@ -1,79 +1,73 @@
 # -*- coding: utf-8 -*-
 
+def base_n(x,n):
+    if x == 0:
+        return '0'
+    nums = []
+    while x:
+        x, r = divmod(x, n)
+        nums.append(str(r))
+    return ''.join(reversed(nums))
+
+def parse_line(line):
+    [test,inputs] = line.split(':')
+    test = int(test)
+    inputs = inputs.split();
+    inputs = [int(x) for x in inputs]
+    return [test,inputs]
+
 #prepare variables ############################################################
 input_path = '..\inputs\day_7.txt'
 txt = open(input_path).read()
 
 lines = txt.split('\n')
 
+lines = [parse_line(x) for x in lines]
+
 #part 1 #######################################################################
 matching = []
-for i,line in enumerate(lines): 
-
-    [test,inputs] = line.split(':')
-    inputs = inputs.split();
+for line in lines: 
+    test = line[0]
+    inputs = line[1];
     n_op = len(inputs)-1;
-    int_vals =  list(range(pow(2,n_op)))
-    bin_vals = [('{0:0' + str(n_op)+ 'b}').format(x) for x in int_vals]
     
-    for j,b in enumerate(bin_vals):
-        o = b.replace('0','+')
-        o = o.replace('1','*')
-        
-        estr = inputs[0]
-        for k in range(len(o)):
-            estr = '('+ estr +o[k]+ inputs[k+1]+')' 
-            
-        test_i = eval(estr)
-            #print(estr)
-            #print(test_i)
-            #print('\n')
-        if test_i == int(test):
-            matching.append(int(test))
+    combos = [base_n(x,2).zfill(n_op) for x in list(range(pow(2,n_op)))]
+    for c in combos:
+        val = inputs[0]
+        for k in range(len(c)):
+            if c[k] == '0': #+ case    
+                val = val + inputs[k+1]
+            elif c[k] == '1': #* case
+                val = val * inputs[k+1]
+            if val > test:
+                break
+        if test == val:
+            matching.append(val)
             break
 
-            
 print(sum(matching))
 
 #part 2 #######################################################################
-def base3(n):
-    if n == 0:
-        return '0'
-    nums = []
-    while n:
-        n, r = divmod(n, 3)
-        nums.append(str(r))
-    return ''.join(reversed(nums))
-
-
 matching = []
 for i,line in enumerate(lines): 
-    
-    [test,inputs] = line.split(':')
-    inputs = inputs.split();
+    test = line[0]
+    inputs = line[1];
     n_op = len(inputs)-1;
-    int_vals =  list(range(pow(3,n_op)))
     
-    bin_vals = [base3(x).zfill(n_op) for x in int_vals]
-    
-    for j,b in enumerate(bin_vals):
-        o = b.replace('0','+')
-        o = o.replace('1','*')
-        o = o.replace('2','|')
-        
-        estr = inputs[0]
-        for k in range(len(o)):
-            if not o[k] == '|':
-                estr = '('+ estr +o[k]+ inputs[k+1]+')' 
-            else:
-                estr = eval('(str('+ estr + ') + str(' + inputs[k+1]+'))')
-
-        test_i = eval(estr)
-      
-        if test_i == int(test):
-            matching.append(int(test))
+    combos = [base_n(x,3).zfill(n_op) for x in list(range(pow(3,n_op)))]
+    for c in combos:
+        val = inputs[0]
+        for k in range(len(c)):
+            if c[k] == '0': #+ case    
+                val = val + inputs[k+1]
+            elif c[k] == '1': #* case  
+                val = val * inputs[k+1]
+            elif c[k] == '2': #|| case
+                val = int(str(val) + str(inputs[k+1]))
+            if val > test:
+                break
+        if test == val:
+            matching.append(val)
             break
-    #print('\n\n')
-    print(str(i) + ' of ' + str(len(lines)))
         
 print(sum(matching))
