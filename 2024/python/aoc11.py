887ed89cb7a66555d9bbc73e51d82df50ed7bf64
bdunne <bdunne6@gmail.com>
2024-12-10 22:19:54 -0800

    Midly golfify 2024 day 11 and add encrypted input.

--- 2024/python/aoc11.py
+++ 2024/python/aoc11.py
@@ -1,36 +1,25 @@
+from collections import Counter
 #prepare variables ############################################################
 input_path = '..\inputs\day_11.txt'
-txt = open(input_path).read()
-#lines = txt.split('\n')
-X = txt.split()
+X  = open(input_path).read().split()
 X = [int(x) for x in X]
 
+#functions ####################################################################
+def count_stones(X,n):
+    X = Counter(dict(zip([int(x) for x in X],[1]*len(X))))
+    for j in range(n):
+        for x,v in list(X.items()):
+            X[x] -= v
+            nd = len(str(x))
+            if x == 0:
+                X[1]+=v
+            elif nd%2 == 0:
+                X[int(str(x)[:int(nd/2)])] += v
+                X[int(str(x)[int(nd/2):])] += v
+            else:
+                X[x*2024] += v
+    return sum(X.values())
 #part 1 #######################################################################
-for j in range(25):
-    for i,x in enumerate(X):
-        nd = len(str(x))
-        if x == 0:
-            X[i] = [1]
-        elif nd%2 == 0:
-            X[i] = [int(str(x)[:int(nd/2)]),int(str(x)[int(nd/2):])]
-        else:
-            X[i] = [X[i]*2024]
-    X = [x for xs in X for x in xs]
-print(len(X))
-
+print(count_stones(X, 25))
 #part 2 #######################################################################
-from collections import Counter
-X = txt.split()
-X = Counter(dict(zip([int(x) for x in X],[1]*len(X))))
-for j in range(75):
-    for x,v in list(X.items()):
-        nd = len(str(x))
-        X[x] -= v
-        if x == 0:
-            X[1]+=v
-        elif nd%2 == 0:
-            X[int(str(x)[:int(nd/2)])] += v
-            X[int(str(x)[int(nd/2):])] += v
-        else:
-            X[x*2024] += v
-print(sum(X.values()))
+print(count_stones(X, 75))
