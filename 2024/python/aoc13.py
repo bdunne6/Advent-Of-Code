7b90cb6f0e47dd537823db05f03c1cd59c00d986
bdunne <bdunne6@gmail.com>
2024-12-13 23:45:17 -0800

    Condense 2024 day 13.

--- 2024/python/aoc13.py
+++ 2024/python/aoc13.py
@@ -1,38 +1,22 @@
 # -*- coding: utf-8 -*-
-import numpy as np
-
+#functions ####################################################################
+def calc_cost(m_txt,offset):
+    c = 0    
+    for m in m_txt:
+        mp = m.split()
+        [xA,yA,xB,yB] = (int(mp[i].split('+')[1].strip(',')) for i in [2,3,6,7])
+        [xP,yP] = (int(mp[i].split('=')[1].strip(','))+offset for i in [9,10])
+        [nA,nB] = ((xP*yB-xB*yP)/(xA*yB-xB*yA),(xP*yA-xA*yP)/(xB*yA-xA*yB))
+        if int(nA) == nA and int(nB) == nB:
+            c += nA*3 + nB
+    return int(c)
+            
 #prepare variables ############################################################
 input_path = '..\inputs\day_13.txt'
 txt = open(input_path).read()
 m_txt = txt.split('\n\n')
 
-
-mlist = []
-for m in m_txt:
-    mp = m.split()
-    xA = mp[2].split('+')[1].strip(',')
-    yA = mp[3].split('+')[1].strip(',')
-    xB = mp[6].split('+')[1].strip(',')
-    yB = mp[7].split('+')[1].strip(',')
-    xP = mp[9].split('=')[1].strip(',')
-    yP = mp[10].split('=')[1].strip(',')
-    mlist.append({'A':(int(xA),int(yA)),'B':(int(xB),int(yB)),'P':(int(xP)+10000000000000,int(yP)+10000000000000)})
-    
-c = 0
-for m in mlist:
-    x1 = m['A'][0]
-    y1 = m['A'][1]
-    x2 = m['B'][0]
-    y2 = m['B'][1]
-    x3 = m['P'][0]
-    y3 = m['P'][1]
-    
-    nA = (x3*y2-x2*y3)/(x1*y2-x2*y1)
-    nB = (x3*y1-x1*y3)/(x2*y1-x1*y2)
-    
-    if int(nA) == nA and int(nB) == nB:
-        c += nA*3 + nB
-
-print(c)
-    
-    
+#part1 ########################################################################
+print(calc_cost(m_txt,0))
+#part2 ########################################################################
+print(calc_cost(m_txt,10000000000000))
