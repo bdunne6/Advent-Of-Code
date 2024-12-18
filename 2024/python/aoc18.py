e2a4fdba85ea81e793f6d997df2ee497639baded
bdunne <bdunne6@gmail.com>
2024-12-17 23:51:34 -0800

    Minor cleanup.

--- 2024/python/aoc18.py
+++ 2024/python/aoc18.py
@@ -11,9 +11,8 @@ b = [tuple(int(x) for x in line.split(',')) for line in lines]
 size = [71,71]
 p = list(product(range(size[0]),range(size[1])))
 d = set([(-1,0),(1,0),(0,-1),(0,1)])
-#functions 
-def build_graph(p1):
-    G = nx.Graph()
+#functions ####################################################################
+def build_graph(p1,G=nx.Graph()):
     for px,py in p1:
         for dx,dy in d:
             pn = (px + dx,py + dy)
@@ -27,7 +26,7 @@ G = build_graph(set(p).difference(b[:i_t]))
 sp = nx.shortest_path(G, (0,0),(size[0]-1,size[1]-1))
 print(len(sp)-1)
 
-# #part 2 #######################################################################
+# #part 2 #####################################################################
 p1 = set(p).difference(b)
 G = build_graph(p1)
 
