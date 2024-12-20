64ede1194fdcd55b63575413c3aca14e93c26d52
bdunne <bdunne6@gmail.com>
2024-12-20 00:07:32 -0800

    Simplify 2024 day 20.

--- 2024/python/aoc20.py
+++ 2024/python/aoc20.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from itertools import product
-import networkx as nx
-import matplotlib.pyplot as plt
 
 #functions
+def get_path(p,pc,e):
+    d = [(-1,0),(1,0),(0,-1),(0,1)]
+    sp = [pc]
+    sps = set([pc])
+    while pc != e:
+        for dx,dy in d:
+            pn = (pc[0] + dx,pc[1] + dy)
+            if pn in p and not pn in sps:
+                sps.add(pn)
+                sp.append(pn)
+                pc = pn
+    return sp
+
 def find_nonzero(X):
     p = X.nonzero()
     p = list(zip(p[0].tolist(),p[1].tolist()))
@@ -18,7 +28,6 @@ def time_saved(p,cheat_time):
             if (mdist <= cheat_time):
                     lsave.append(j-i-mdist)
     return lsave
-            
 
 #prepare variables ############################################################
 input_path = '..\inputs\day_20.txt'
@@ -29,26 +38,15 @@ X = np.zeros((len(lines),len(lines[0])))
 for i,line in enumerate(lines):
     X[i,:] = np.array([ord(c) for c in line]) #numeric into padded array
     
-
-
 s = find_nonzero(X == 83)[0]
 e = find_nonzero(X == 69)[0]
 X[s] = 46
 X[e] = 46
-
 p = set(find_nonzero(X == 46))
-d = set([(-1,0),(1,0),(0,-1),(0,1)])
 
-G = nx.Graph()
-for px,py in p:
-    for dx,dy in d:
-        pn = (px + dx,py + dy)
-        if pn in p:
-            G.add_edge((px,py),pn,weight =1)
-
-sp = nx.shortest_path(G, s,e)
+sp = get_path(p,s,e)
 #part 1 #######################################################################
 print(sum(np.array(time_saved(sp,2))>=100))
 
 #part 2 #######################################################################
-print(sum(np.array(time_saved(sp,2))>=100))
+print(sum(np.array(time_saved(sp,20))>=100))
