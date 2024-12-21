e0f7cfa264ab13b4662ab2e229415b1d5d9aefcf
bdunne <bdunne6@gmail.com>
2024-12-20 22:54:39 -0800

    Fix 2024 day 19 part 1 graph solution.

--- 2024/python/aoc19.py
+++ 2024/python/aoc19.py
@@ -3,6 +3,7 @@ import numpy as np
 import re
 import networkx as nx
 
+#prepare variables ############################################################
 input_path = '..\inputs\day_19.txt'
 txt = open(input_path).read()
 lines = txt.split('\n')
@@ -12,39 +13,22 @@ tpat = [s.strip() for s in tpat]
 
 designs = lines[2:]
 
-# pattern = '^(' + ('|'.join(tpat))+ ')*$'
-# p = re.compile(pattern)
-
+#part 1 #######################################################################
 nm = 0
-# for i,d in enumerate(designs):
-      
 for d in designs:
-    #d = designs[0]
     dse = {}
     for p in tpat:
         m = re.finditer(p,d)
         if m:
             for mi in m:
-                dse.setdefault(mi.start(), []).append(mi.end()-1)
-                #print(mi)
-                #mse.add((mi.start(),mi.end()-1))
-        
+                dse.setdefault(mi.start(), []).append(mi.end())
     G = nx.DiGraph()
     for s,e in dse.items():
         for ei in e:
-            if ei !=s:
-                G.add_edge(s,ei)
-            if ei+1 in dse:
-                G.add_edge(ei,ei+1)
-    
-        
-    nm += (0 in G and (len(d)-1) in G and nx.has_path(G,0,len(d)-1))
-    print((0 in G and len(d)-1 in G and nx.has_path(G,0,len(d)-1)))
-    # nm += bool(p.match(d))
-    
-
+            G.add_edge(s,ei)
+    nm += (0 in G and (len(d)) in G and nx.has_path(G,0,len(d)))
 print(nm)
-
+#part 2 #######################################################################
 
 nx.draw(G,with_labels=True)
 
